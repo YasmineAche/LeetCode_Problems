424e63702b002686c55c9204493b8877f9f18680
Yasmine Achemine <128965544+YasmineAche@users.noreply.github.com>
2023-10-18 20:43:41 +0100

    Time: 1848 ms (47.26%), Space: 13.9 MB (97.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,12 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        result = []
         for index, num in enumerate(nums):
             nums[index]= ""
             for i, _ in enumerate(nums[index+1:]):
                 if num + _ == target :
-                    result.append(index)
-                    result.append(nums.index(_))
-                    return result
+                    return [index, nums.index(_)]
             
